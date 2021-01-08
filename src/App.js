a7b96a48bca7a77224335fd8a7128abf85610296
BSzwimer <benji.szwimer@gmail.com>
2021-01-08 09:20:05 -0500

    personal webpage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Particles from "react-particles-js";
+import particlesConfig from "./config/particlesConfig";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: "100%", heigh: "100vh", backgroundColor: "black" }}>
+      <Particles height="100vh" width="100vw" params={particlesConfig} />
     </div>
   );
 }
