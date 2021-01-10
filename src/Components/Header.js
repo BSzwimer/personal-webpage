f2af22684bcde5ee3108de723fed54a1350f16a6
BSzwimer <benji.szwimer@gmail.com>
2021-01-10 13:22:57 -0500

    noreferrer noopener add

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -35,13 +35,17 @@ const Header = () => {
         className="right menu"
         style={{ paddingTop: "10px", paddingRight: "10px" }}
       >
-        <a href="https://github.com/BSzwimer" target="_blank" rel="noreferrer">
+        <a
+          href="https://github.com/BSzwimer"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
           <i className="inverted github icon large"></i>
         </a>
         <a
           href="https://www.linkedin.com/in/benjamin-szwimer-39448a173/"
           target="_blank"
-          rel="noreferrer"
+          rel="noreferrer noopener"
         >
           <i className="inverted linkedin icon large"></i>
         </a>
